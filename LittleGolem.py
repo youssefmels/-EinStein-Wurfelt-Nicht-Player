0053e07540f18c5d0198d2ba8161247b7d5a7eb3
youssefmels <85249386+youssefmels@users.noreply.github.com>
2025-01-11 23:59:01 +0200

    Update LittleGolem.py

--- LittleGolem.py
+++ LittleGolem.py
@@ -1,8 +1,9 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+import re
 
-def download_txt_file(requests, url):
+def download_txt_file(url):
     os.makedirs("downloads", exist_ok=True)
     response = requests.get(url)
 
@@ -27,8 +28,32 @@ def download_txt_file(requests, url):
         print("failed. status code: ", response.status_code)
     pass
 
+def parse_data(file_location):
+    games = []
+
+    with open(file_location, 'r') as f:
+        lines = f.read()
+
+    unclean_games = lines.strip().split('\n\n')
+    
+    for part in unclean_games:
+        game = {}
+        parsed_moves = []
+        matches = re.findall(r'\[(\w+)\s+\"(.*?)\"\]', part)
+
+        for key,value in matches:
+            game[key.lower()] = value
+
+        moves = re.findall(r'\d+/\S+', part)
+        parsed_moves.extend(moves)
+        game["move"] = parsed_moves
+        games.append(game)
+    return games
+
 def main():
     download_txt_file(requests, "https://www.littlegolem.net/jsp/info/player_game_list.jsp?gtid=einstein&plid=10675")
+    path = "C:\\Users\\youss\\Desktop\\DataEngineering\\LittleGolem\\downloads\\match_history.txt"
+    games = parse_data(path)
     pass
 
 
